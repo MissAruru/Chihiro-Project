365b814344ac71196f1516ede07790841c58c250
Aroa Granja Iglesias <aroagranjaiglesias@gmail.com>
2024-10-30 13:35:05 +0100

    Update cloudinary.js

--- chihiro-api/config/cloudinary.js
+++ chihiro-api/config/cloudinary.js
@@ -1,5 +1,10 @@
 const cloudinary = require('cloudinary').v2;
-require('dotenv').config()
+require('dotenv').config();
+
+// Verifica si las variables de entorno están definidas
+if (!process.env.CLOUDINARY_CLOUD_NAME || !process.env.CLOUDINARY_API_KEY || !process.env.CLOUDINARY_API_SECRET) {
+    throw new Error("Asegúrate de que CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY y CLOUDINARY_API_SECRET estén definidos en el archivo .env");
+}
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -12,4 +17,4 @@ cloudinary.api.ping()
     .then(result => console.log("Conexión exitosa a Cloudinary:", result))
     .catch(err => console.error("Error al conectar a Cloudinary:", err));
 
-module.exports = cloudinary
+module.exports = cloudinary;
